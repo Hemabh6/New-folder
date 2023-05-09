18c9825cc9191722851544207bdc233d3600c9c0
adityanilabh <nilabhaditya6@gmail.com>
2023-05-09 16:15:11 +0530

    new addition

--- AniketGst/src/app/app.module.ts
+++ AniketGst/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AniketGst/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+firebase:{
+    apiKey: "AIzaSyCj2FdUSate1njiqoTE6rWmGHU85pwSblM",
+
+  authDomain: "aniketgst-15940.firebaseapp.com",
+
+  projectId: "aniketgst-15940",
+
+  storageBucket: "aniketgst-15940.appspot.com",
+
+  messagingSenderId: "5847150385",
+
+  appId: "1:5847150385:web:e37fcc660c531cfe400a10"
+
+}
+};
